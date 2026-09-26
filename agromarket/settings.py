2230b4b65e57aa6e8b47702838ff92f38c0dccb5
Souleymana <aboubacar.souleymana@gmail.com>
2026-09-26 15:11:12 +0200

    Configurer stockage images Cloudinary

--- agromarket/settings.py
+++ agromarket/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     "orders",
     "core",
     "accounts",
+    "cloudinary",
+    "cloudinary_storage",
 ]
 
 
@@ -228,3 +230,17 @@ STORAGES = {
     },
 }
 
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+}
+
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
